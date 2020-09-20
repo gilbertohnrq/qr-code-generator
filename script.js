bb9729d43e2c8c69b1652632f291e496e7447d86
gilbertohnrq <gilbertohnrq@gmail.com>
2020-09-20 15:38:12 -0300

    a simple qrcode generator

---
+++ script.js
@@ -0,0 +1,19 @@
+let qrcode = select('img');
+let qrtext = select('textarea');
+let qrbutton = select('button');
+
+qrbutton.addEventListener('click', generateQR);
+
+function generateQR() {
+  let size = '1000x1000';
+  let data = qrtext.value;
+  let baseURL = 'http://api.qrserver.com/v1/create-qr-code';
+
+  let url = `${baseURL}?data=${data}&size=${size}`;
+
+  qrcode.src = url;
+}
+
+function select(element) {
+  return document.querySelector(element);
+}
